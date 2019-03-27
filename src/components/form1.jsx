3f0430aa489c557d435b0c0bf963a482fc59446c
Malcolm <malcolm.maclure@gmail.com>
2019-03-27 10:22:31 -0400

    created container and form components

---
+++ src/components/form1.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form1 extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+      </div>
+     );
+  }
+}
+ 
+export default Form1;
---
+++ src/components/form2.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form2 extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <div>
+      </div>
+     );
+  }
+}
+ 
+export default Form2;
