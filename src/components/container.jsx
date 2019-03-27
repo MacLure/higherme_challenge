45c4b5adc1dfee1e03a847dfe5d62d00b73b8348
Malcolm <malcolm.maclure@gmail.com>
2019-03-27 11:43:47 -0400

    added form inputs and some styling

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Form1 from './form1'
+import Form2 from './form2'
+
 
 class Container extends Component {
   state = {  }
   render() { 
     return ( 
       <div>
+        <div>
+          Which form shall I display?
+          <div className="formSelectButton">Form 1</div>
+          <div className="formSelectButton">Form 2</div>
+        </div>
+        <Form1 />
+        <Form2 />
       </div>
      );
   }
--- src/components/form1.jsx
+++ src/components/form1.jsx
@@ -5,6 +5,15 @@ class Form1 extends Component {
   render() { 
     return ( 
       <div>
+        FORM 1
+        <form>
+          <label>
+          </label>
+          <input className="inputText" type="text" placeholder="Text input for Form 1" />
+          <input className="inputText" type="number" placeholder="Number input for Form 1" />
+          <input className="inputText" type="email" placeholder="Email input for Form 1" />
+          <input className="inputText" type="password" placeholder="Password input for Form 1" />
+        </form>
       </div>
      );
   }
